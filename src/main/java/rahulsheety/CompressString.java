205351275da3cfed83cacda33ba0405901385fa5
Abhilekh <abhilekh@saffire.com>
2024-11-12 19:02:16 +0530

    compress

---
+++ src/main/java/rahulsheety/CompressString.java
@@ -0,0 +1,22 @@
+package rahulsheety;
+
+public class CompressString {
+    public static void main(String[] args) {
+        String input = "aabbbcccc";
+        String output = "";
+        int count = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == input.charAt(i - 1)) {
+                count++;
+            } else {
+                output += input.charAt(i - 1) + Integer.toString(count);
+                count = 1;
+            }
+        }
+        output += input.charAt(input.length() - 1) + Integer.toString(count); // For last character
+        System.out.println(output); // Output: a2b3c3
+    }
+}
+
+
