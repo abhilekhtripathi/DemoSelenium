2125e5ec82e8c848f2c891bca0fedc704dec7e0b
Abhilekh <abhilekh@saffire.com>
2024-12-04 18:44:07 +0530

    largest program

---
+++ src/main/java/part1/LargestAndSmallest.java
@@ -0,0 +1,28 @@
+package part1;
+
+public class LargestAndSmallest {
+
+	public static void main(String[] args) {
+		// Initialize the array
+		int[] a = { 3, 5, 7, 2, 8, -6, 4, 10, -22, 15 };
+
+		// Assume the first element is both the largest and smallest
+		int largest = a[0];
+		int smallest = a[0];
+
+		// Traverse through the array
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > largest) {
+				largest = a[i];
+			} else if (a[i] < smallest) {
+				smallest = a[i];
+			}
+		}
+
+		// Output the result
+		System.out.println("Largest Number: " + largest);
+		System.out.println("Smallest Number: " + smallest);
+
+	}
+
+}
