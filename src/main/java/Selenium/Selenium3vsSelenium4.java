93f2b171c4e86a5e27bc2f97892e171db8f49f22
Abhilekh <abhilekh@saffire.com>
2025-01-13 18:17:53 +0530

    13 jan updated

---
+++ src/main/java/Selenium/Selenium3vsSelenium4.java
@@ -0,0 +1,28 @@
+package Selenium;
+
+public class Selenium3vsSelenium4 {
+
+	public static void main(String[] args) 
+	{
+		//  Selenium 4
+		/**
+		 * Selenium 4 uses W3C standard protocol	
+		 * Chrome Driver class extends chromium driver class	
+		 * Optimised Selenium Grid with enhanced GUI and support for Docker	
+		 * Enhanced Selenium IDE with improved GUI and cloud based selenium grid	
+		 * Testers need not start the Hub and Node jars everytime they perform automation testing using Selenium Grid.	
+		 * 
+		 * Selenium 3
+		 * Selenium 3 used JSON wire protocol
+		 * Chrome Driver class extended Remote webdriver class
+		 * No Support for docker
+		 * Selenium IDE just available as a firefox add-on
+		 * Testers always had to start Hub and Node jars which was a difficult task in selenium 3.
+		 * 
+		 * 
+		 * 
+		 */
+
+	}
+
+}
---
+++ src/main/java/durgaSir/Constructor.java
@@ -0,0 +1,26 @@
+package durgaSir;
+
+public class Constructor 
+{
+/**
+ * The name of the constructors must be the same as the class name.
+ *  Java constructors do not have a return type. Even do not use void as a return type.
+ * There can be multiple constructors in the same class, this concept is known as constructor overloading.
+ * The access modifiers can be used with the constructors, use if you want to change the visibility/accessibility of constructors.
+ *
+ * Java provides a default constructor that is invoked during the time of object creation.
+ *  If you create any type of constructor, the default constructor (provided by Java) is not invoked.
+ * 
+ * types
+ * Default Constructor
+  No-Args Constructor
+  Parameterized Constructor
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+}
