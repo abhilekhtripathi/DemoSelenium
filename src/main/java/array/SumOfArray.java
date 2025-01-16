82f91765cd0ea4d890cbe6ca35fc771d437fad3e
Abhilekh <abhilekh@saffire.com>
2025-01-16 14:31:00 +0530

    updated programs

---
+++ src/main/java/array/SumOfArray.java
@@ -0,0 +1,14 @@
+package array;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		int[] arr = {12, 16, 12, 34, 5, 38, 35};
+
+        // Find 12 and 38 and sum them
+        int sum = arr[1] + arr[3]; // arr[0] is 12 and arr[5] is 38
+        System.out.println("Sum of : " + sum);
+
+	}
+
+}
