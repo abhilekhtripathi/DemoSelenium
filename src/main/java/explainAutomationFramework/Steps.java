b302b30fdc2768fe5eb9ba22b6cbcf66a341c332
Abhilekh <abhilekhtripathi8@gmail.com>
2025-01-20 13:52:52 +0530

    updated for the automation framework

---
+++ src/main/java/explainAutomationFramework/Steps.java
@@ -0,0 +1,44 @@
+package explainAutomationFramework;
+
+public class Steps 
+{
+
+	/**
+	 * "Our Selenium framework is a robust test automation solution designed using 
+	 * Java, Selenium WebDriver, Page Object Model (POM), TestNG, Data-Driven Testing, and Reporting. 
+	 * The framework is modular, scalable, and ensures separation of concerns, making it maintainable and reusable."
+	 * 
+	 * Key Components of the Framework:----
+	 *  Page Object Model (POM)
+	 *  TestNG for Test case's Management
+	 *  Data-Driven concept for managing the test data
+	 *  Reporting for view test cases report 
+	 *  Reusable/driver Utilities
+	 *  
+	 *  This framework is modular, extensible, and designed to handle complex testing requirements. 
+	 *  It enables high test coverage, faster debugging through detailed reports, 
+	 *  and easy integration with CI/CD pipelines for continuous testing."
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+}
